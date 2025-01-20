ff47d0470a0a4e5f0d22c140ab981ac4cf01cf62
MizAless <44200635+MizAless@users.noreply.github.com>
2025-01-20 16:17:41 +0700

    Update UnitSpawner.cs

--- Assets/_Game/Scripts/Unit/UnitSpawner.cs
+++ Assets/_Game/Scripts/Unit/UnitSpawner.cs
@@ -1,33 +1,33 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class UnitSpawner : Spawner<Unit>
 {
-    [field: SerializeField] public float Cooldown = 1f;
-    // [SerializeField] private int _maxUnits = 1;
-    // [SerializeField] private Base _base;
-    
-    private bool _canSpawn = true;
+    [SerializeField] private float _spawnCooldown = 1f;
+    [SerializeField] private int _maxUnits = 1;
+    [SerializeField] private Base _base;
 
     private int _currentCount = 0;
 
     public void Init()
     {
-        _canSpawn = false;
-
-        yield return new WaitForSeconds(Cooldown);
-        
-        _canSpawn = true;
+        StartCoroutine(Spawning());
     }
 
-    public override Unit Spawn()
+    private IEnumerator Spawning()
     {
-        if (_canSpawn == false)
-            return null;
+        var wait = new WaitForSeconds(_spawnCooldown);
 
-        StartCoroutine(CooldownProcess());
-        
-        return base.Spawn();
+        while (enabled)
+        {
+            if (_currentCount < _maxUnits)
+            {
+                Unit spawnedUnit = base.Spawn();
+                spawnedUnit.Init(_base);
+                _currentCount++;
+            }
+
+            yield return wait;
+        }
     }
 }
